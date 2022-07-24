0f71581b3cc73e12092a73ff19e64efcf24b651d
limminjeong98 <limminjeong98@naver.com>
2022-07-24 15:56:30 +0900

    도메인 분석 설계

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
+    private Order order;
+    // Order.delivery 필드와 매핑됨
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
+
+}
